4a0dfb8d51bef219ae31d5681bd2f46ebf7d9384
Ilnur <nabiulli@mail.ru>
2022-06-06 22:12:51 +0500

    Выполнил домашнее задание по 3.2. Создание API. Swagger UI, Postman

---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,46 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import org.webjars.NotFoundException;
+import ru.hogwarts.school.model.Faculty;
+
+import java.util.*;
+
+@Service
+
+public class FacultyService {
+    private Map<Long, Faculty> faculties = new HashMap<>();
+    private Long generatedFacultyId = 1L;
+
+    public Faculty createFaculty(Faculty faculty) {
+        faculties.put(generatedFacultyId, faculty);
+        generatedFacultyId++;
+        return faculty;
+    }
+
+    public Faculty getFacultyById(Long facultyId) {
+        return faculties.get(facultyId);
+    }
+
+    public Faculty updateFaculty(Long facultyId, Faculty faculty) {
+        faculties.put(generatedFacultyId, faculty);
+        return faculty;
+    }
+
+    public Faculty deleteFaculty(Long facultyId) {
+        return faculties.remove(facultyId);
+    }
+
+    public Set<Faculty> findFacultiesByColor(String colour) {
+        Set<Faculty> facultiesWithFindingColour = new HashSet<>();
+        for (Faculty faculty : faculties.values()) {
+            if (faculty.getColor().equals(colour)) {
+                facultiesWithFindingColour.add(faculty);
+            }
+        }
+        if (facultiesWithFindingColour == null) {
+            throw new NotFoundException("факультетов с таким цветом нет");
+        }
+        return facultiesWithFindingColour;
+    }
+}
